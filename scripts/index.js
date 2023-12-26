9666fc2a78947ee2e6eed42882efaaee56b58112
Yulia Basalaeva <y.basalaevaaa@yandex.ru>
2023-12-26 12:26:14 +0300

    удаление карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,25 @@
 const container = document.querySelector('.content');
 const cardsContainer = container.querySelector('.places__list');
 
-function addCard(elements) {
+function createCard(element, deleteElement) {
   const cardTemplate = document.querySelector('#card-template').content;
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
   const deleteButton = cardElement.querySelector('.card__delete-button');
-  cardElement.querySelector('.card__image').src = elements.link;
-  cardElement.querySelector('.card__title').textContent = elements.name;
-  cardsContainer.append(cardElement);
 
-  deleteButton.addEventListener('click', function () {
-    cardElement.remove();
-  }); 
+  cardElement.querySelector('.card__image').src = element.link;
+  cardElement.querySelector('.card__title').textContent = element.name;
+  cardElement.querySelector('.card__image').alt = element.name;
+
+  deleteButton.addEventListener('click', () => deleteElement(cardElement));
+
+  return cardElement;
 }
 
 initialCards.forEach(item => {
-  const card = addCard(item);
+  const card = createCard(item, deleteCard);
+  cardsContainer.append(card);
 })
+
+function deleteCard(cardElement) {
+  cardElement.remove();
+} 
